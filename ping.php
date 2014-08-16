7df36f4000cbfbc63a6220549e17b873dfceb1c3
masted <masted311@gmail.com>
2014-08-16 16:06:46 +0400

    Commit was made from server localhost by ci/push

--- ping.php
+++ ping.php
@@ -1,5 +1,5 @@
 <?php
 
-$id = Misc::checkEmpty($_SERVER['argv'][2], 'param #2 (id) is required');
+$id = R::get('options')['id'];
 (new Image)->resizeAndSave(__DIR__.'/web/captures/'.$id.'.png', __DIR__.'/web/captures/'.$id.'.png', 340, 220);
 if (($client = (new WsClient('localhost', 9000))->connect(false)) !== false) $client->sendData("capture:/captures/$id.png");
