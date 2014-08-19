34b32c57e62e442c7065f178aed52555bfe959bb
masted <masted311@gmail.com>
2014-08-20 00:14:45 +0400

    Commit was made from server localhost by ci/push

--- install.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-(new DaemonInstaller('rumax', 'wss', [
-  'opts' => '/home/user/ngn-env/run/run.php rumax/wss'
-]))->install();
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-print "1111111 ".get_current_user();
-//(new WsClient('localhost', 9000))->connect()->sendData(Misc::randString());
-//$c = new WsClient2('localhost', 9000);
-//$c->connect('localhost', 9000, '/');
-//$c->sendData('dwqdqw');
